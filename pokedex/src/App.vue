fbd95a280f862e073c37faef743cb46dd4de0e32
Matheus Ghotme <51427002+nazuki8540@users.noreply.github.com>
2022-10-17 21:29:15 -0300

    newFile

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-
+    <div v-for="(poke,index) in pokemons" :key="index">
+      <h1>{{ index + 1 }} {{poke.name}}</h1>
+    </div>
   </div>
 </template>
 <script>
@@ -15,15 +17,13 @@ export default {
   created: function(){
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=152&offset=0").then(res => {
       console.log("Pegou a lista de pokemons");
-      this.pokemns = res.data.results;
-    }).catch(err => {
-      console.log(err);
+      this.pokemons = res.data.results;
     })
   }
 }
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -31,7 +31,4 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-
-
-
 </style>
